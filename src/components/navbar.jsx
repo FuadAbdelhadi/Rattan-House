e9d95a6af461fbd44d84cc5b4e40c329f4ca27f9
Fuad Abdelhadi <fuadrabdelhadi@gmail.com>
2025-11-10 22:39:04 +0300

    fixed an issue with the static images url

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
       <nav className="navbar navbar-inner-container navbar-expand-lg navbar-light">
         <div className="container-fluid">
           <Link className="navbar-brand navbar-brand-mobile" to="/">
-            <img src={isProductDetailsPage ? "/images//footer logo.svg" : "/images//logo 1.svg"}  alt="Logo" />
+            <img src={isProductDetailsPage ? "/images/footer logo.svg" : "/images/logo 1.svg"}  alt="Logo" />
           </Link>
 
           <button
@@ -49,7 +49,7 @@ const Navbar = () => {
             id="navbarSupportedContent"
           >
             <Link className="navbar-brand" to="/">
-              <img className="header-logo" src={isProductDetailsPage ? "/images//footer logo.svg" : "/images//logo 1.svg"}  alt="Logo" />
+              <img className="header-logo" src={isProductDetailsPage ? "/images/footer logo.svg" : "/images/logo 1.svg"}  alt="Logo" />
             </Link>
 
             <ul className="navbar-nav mb-2 mb-lg-0">
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/Rattan-House/",
+  base: "/Rattan-House/",
   // server: {
   //   proxy: {
   //     "/api": {
